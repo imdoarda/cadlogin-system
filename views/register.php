97f438d76fa617785d2629e5528e5f217d4f8ec0
imdoarda <dudinha.mgalvao@gmail.com>
2024-09-17 07:54:34 -0300

    adiciona labels e botão de cadastro

--- views/register.php
+++ views/register.php
@@ -9,7 +9,22 @@
     <div>
         <h2>Cadastro de Usuário</h2>
         <form action="" method="post">
-            <label for=""></label>
+            <label for="">Nome</label>
+            <input type="text" name="nome" id="nome" required>
+
+            <label for="">Email</label>
+            <input type="email" name="email" id="email" required>
+
+            <label for="">Senha</label>
+            <input type="password" name="senha" id="senha" required>
+
+            <label for="">Perfil:</label>
+            <select name="perfil" id="perfil">
+                <option value="admin">Admin</option>
+                <option value="gestor">Gestor</option>
+                <option value="colaborador">Colaborador</option>
+            </select>
+            <button type="submit">Cadastrar</button>
         </form>
     </div>
 </body>
