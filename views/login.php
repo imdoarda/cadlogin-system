13dfcb624b6f8c4094e7ce63ece11909a7697d23
imdoarda <dudinha.mgalvao@gmail.com>
2024-09-17 07:31:24 -0300

    inicia tela login

---
+++ views/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <main>
+        <form action="">
+            <section>
+                <label for="">Email</label>
+                <input type="email" name="email" placeholder="email" required>
+            </section>
+            <section>
+
+            </section>
+        </form>
+    </main>
+</body>
+</html>
