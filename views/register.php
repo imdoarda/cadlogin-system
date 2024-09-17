39abb21e75246bc9419659a3cecec75fef9b8f1b
imdoarda <dudinha.mgalvao@gmail.com>
2024-09-17 07:45:16 -0300

    inicia tela de cadastro

---
+++ views/register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastre-se</Cadastre-se></title>
+</head>
+<body>
+    <div>
+        <h2>Cadastro de Usuário</h2>
+        <form action="" method="post">
+            <label for=""></label>
+        </form>
+    </div>
+</body>
+</html>
