99318cc695b842a1e34af980907c6343c18f9450
imdoarda <dudinha.mgalvao@gmail.com>
2024-09-24 08:11:54 -0300

    model p/ encontrar usuário p/ email

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -8,6 +8,13 @@ class AuthController
     public function login()//faz conexão c/ banco de dados
     {
         //verifica se a requisição http é do tipo POST, ou seja, se o formulário foi enviado
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //obter os valores do formulário
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+
+            //chama o método do model para encontrar o usuário pelo email
+        }
     }
 }
 ?>
