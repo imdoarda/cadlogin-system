69b20fab7b2a7067ae9c9ca299e79b4b0334c4da
imdoarda <dudinha.mgalvao@gmail.com>
2024-09-30 11:36:41 -0300

    correção bdd

--- models/database.php
+++ models/database.php
@@ -1,25 +1,29 @@
 <?php
-//Define classe database, responsável por gerenciar a conexão com banco de dados
-class Database{
-        //  Padrão Singleton, cujo objetivo é garantir que apenas umas instância de classe seja criada durante a execução do programa, e que essa instância seja reutilizada sempre que necessário
-        private static $instance = null;
- 
-        public static function getConnection(){
- 
-            if(!self::$instance){
-                // Configurações de conexão com BD
-                $host           = 'localhost';
-                $db             = 'sistema_usuarios';
-                $user           = 'root';
-                $password       = '';
-               
-                // A conexão usa o driver MYSQL (mysql:) e as informações de host e BD
-                self::$instance = new PDO("mysql:$host;dbname=$db", $user, $password);
- 
-                //Define o modo de erro para exceções, facilitando a depuração e tratamento do erro
-                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-            return self::$instance;
+
+// Define classe Database, responsável por gerenciar a conexão com banco de dados
+class Database
+{
+    // Padrão Singleton, cujo objetivo é garantir que apenas uma única instância de classe seja criada durante a execução do programa, e que essa instância seja reutilizada sempre que necessário
+    private static $instance = null;
+
+    public static function getConnection()
+    {
+
+        if (!self::$instance) {
+            // Configurações de conexão com BD
+            /*
+            $host       = 'localhost';
+            $db         = 'sistema_usuarios';
+            $user       = 'root';
+            $password   = '';
+ */
+            $dsn = "mysql:host=localhost;dbname=sistema_usuarios";
+            // A conexão usa o driver Mysql (mysql:) e as informações de host e BD
+            //self::$instance = new PDO("mysql:$host;dbname=$db", $user, $password);
+            self::$instance = new PDO($dsn, "root", "");
+            //Define o modo de erro para exceções, facilitando a depuração e tratamento do erro
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
+        return self::$instance;
+    }
 }
-?>
--- routes.php
+++ routes.php
@@ -1,20 +1,26 @@
 <?php
 
 //inclui arquivos de controlador necessários para lidar com diferentes ações
-require 'controllers/AuthController.php';// inclui o controlador de autenticação
+require 'controllers/AuthController.php'; // inclui o controlador de autenticação
 require 'controllers/UserController.php'; // inclui o controlador de usuários
-require 'controllers/DashboardController.php'; //inclui o controlador de dashboard
+//require 'controllers/DashboardController.php'; //inclui o controlador de dashboard
 
 //cria instâncias dos controladores para utilizar seus métodos
 $authController = new AuthController(); // Instancia controlador de autenticação
-$userController = new UserController();// Instancia controlador de usuário
-$dashboardController = new DashboardController(); // Instancia controlador dashboard
+$userController = new UserController(); // Instancia controlador de usuário
+//$dashboardController = new DashboardController(); // Instancia controlador dashboard
 
 //coleta a ação de URL, se não houver ação definida, usa 'login' como padrão
 $action = $_GET['action'] ?? 'login'; //usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente
 
-switch($action){
+switch ($action) {
     case 'login':
-        $authController->login();// chama o método de longin do controlador de autentificação
+        $authController->login(); // chama o método de longin do controlador de autentificação
+        break;
+    case 'register':
+        $userController->register();
+        break;
+    default:
+        $authController->login();
+        break;
 }
-?>
