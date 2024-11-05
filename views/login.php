cc9e0f3f9cbdfaf29443c279756d19cb7b1db6d5
imdoarda <dudinha.mgalvao@gmail.com>
2024-11-05 09:28:46 -0300

    conecta css e index

--- views/login.php
+++ views/login.php
@@ -3,20 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/login.css">
     <title>Login</title>
 </head>
 <body>
     <main>
         <form action="index.php?action=login" method="post">
-            <!--cria duas seções diferentes-->
+            <!-- cria duas seções diferentes -->
             <section>
-                <label for="">Email</label>
-                <input type="email" name="email" placeholder="email" required>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Email" required>
             </section>
             <section>
-                <label for="">Senha</label>
-                <input type="password" name="senha" placeholder="Senha" required>
+                <label for="senha">Senha</label>
+                <input type="password" id="senha" name="senha" placeholder="Senha" required>
             </section>
             <button type="submit">Login</button>
         </form>
